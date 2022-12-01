4805070fea8b8048315ef9af9f89c0ea1769aa32
robertdpowell <robertdpowell@gmail.com>
2022-12-01 09:59:00 +0000

    Initial commit

---
+++ 01/solution.py
@@ -0,0 +1,13 @@
+with open("input.py") as f:
+    calories = []
+    for line in f.read().split("\n\n"): #split the list into groups separated by blank lines
+        elf = list(map(int, line.split("\n"))) #convert each string in group into ints
+        elfCals = sum(elf) #sum each group
+        calories.append(elfCals) #add each sum to a list
+
+calories.sort(reverse=True)
+maxThree = sum(calories[:3])
+
+
+print (f'solution 1 answer = {(max(calories))}')
+print (f'solution 2 answer = {(maxThree)}')
