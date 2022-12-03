ef88357de8a18ee265e6d38a99bbea7db3a351f8
robertdpowell <robertdpowell@gmail.com>
2022-12-03 10:32:11 +0000

    day 3

--- 03/solution.py
+++ 03/solution.py
@@ -2,22 +2,23 @@ with open('input.py') as f:
     bps = [x.strip() for x in f.readlines()]
     groupedbps = [bps[i:i+3] for i in range(0, len(bps), 3)]
 
+    def convert(matches):
+        # The ord() function returns the number representing the unicode code of a specified character.
+        lowerPrio = list(map(int, [ord(x) - 96 for x in matches if x.islower()]))
+        upperPrio = list(map(int, [ord(x) - 38 for x in matches if x.isupper()]))
+        return (sum(lowerPrio + upperPrio))
+
     def solve1():
         matches = []
         for i in bps:
             # split in half and remove the duplicates
             h1 = set(i[:len(i) // 2])
             h2 = set(i[len(i) // 2:])
-
             for i in h1:
                 for j in h2:
                     if i == j:
                         matches.append(i)
-
-        #The ord() function returns the number representing the unicode code of a specified character.
-        lowerPrio = list(map(int, [ord(x) - 96 for x in matches if x.islower()]))
-        upperPrio = list(map(int, [ord(x) - 38 for x in matches if x.isupper()]))
-        return (sum(lowerPrio + upperPrio))
+        return (convert(matches))
 
     def solve2():
         matches = []
@@ -30,9 +31,7 @@ with open('input.py') as f:
                     for k in third:
                         if i == j == k:
                             matches.append(i)
-        lowerPrio = list(map(int, [ord(x) - 96 for x in matches if x.islower()]))
-        upperPrio = list(map(int, [ord(x) - 38 for x in matches if x.isupper()]))
-        return (sum(lowerPrio + upperPrio))
+        return (convert(matches))
 
 print (f' Solution 1 answer is {solve1()}')
 print (f' Solution 2 answer is {solve2()}')
